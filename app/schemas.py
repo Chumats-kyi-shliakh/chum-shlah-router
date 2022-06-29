07743cd7ef2fd1859a3d46e73c27852137bb18c5
Denis Kovshun <deniskovshun@gmail.com>
2022-06-29 15:16:04 +0300

    Fics default values for orders array

--- app/schemas.py
+++ app/schemas.py
@@ -8,6 +8,5 @@ class RouteQuery(BaseModel):
     longitude: float = 28.66468
     latitude: float = 50.26009
     storage_ids: List[int] = [4378, 3914, 4008]
-    order_ids: List[UUID] = ['b8144902-fb7d-44db-9842-a3df79a7b88e',
-                             '9a134cba-092d-4ac6-ba19-6130f73b51df',
-                              '31a84b4b-cfc7-48a3-9ad4-8695f71a9ab9']
+    order_ids: List[UUID] = ['f1bb12f0-8e39-4846-a532-e7c2aaf1731a', 'c920831a-ef35-4d54-8587-765cca60832f',
+                             '543870d0-3609-4e0d-a346-797cc1c814ae', 'c3834b6f-8330-4327-8de8-f98b8f3ac04f']
